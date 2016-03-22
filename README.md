c38e3bf664c7eb96dc611944617e8d497d302015
Miltos Pistiolas <pistiolasm@outlook.com>
2016-03-21 21:28:12 -0400

    updated database & api calls

--- README.md
+++ README.md
@@ -1,18 +1,14 @@
-<<<<<<< HEAD
-[Database Map Area (IDs)](http://www.wit.edu/reslife/images/photos/Misc/campus%20map.jpg)
+# API Examples:
 
-API Examples:
-## `http://localhost:3000/api/area`
-* Area: building, quad, parkinglot
+ Get index or type (accessed at GET http://localhost:3000/api/:choice)
+ - `/api/index` to list everything
+ - `/api/vulnerability`
+ - `/api/building`
+ - `/api/quad`
+ - `/api/parkinglot`
 
-## `http://localhost:3000/api/area/id`
-=======
-IDs used in database: [Map Area](http://www.wit.edu/reslife/images/photos/Misc/campus%20map.jpg)
+Get area by id (accessed at GET http://localhost:3000/api/area/:id)
+ - `/api/area/:id` (EXAMPLE: 555HA)
 
-API Examples:
-#### `http://localhost:3000/api/area`
-* Area: building, quad, parkinglot
-
-#### `http://localhost:3000/api/area/id`
->>>>>>> eb12c1155fadc7c5440a593c73646d967dfebc08
-* ID: Building ID 525HA for 535 Hangtington Ave.
+List areas by vulnerability type and severity level (accessed at GET http://localhost:3000/api/vulnerability/:type/:level)
+ - `/api/vulnerability/:type/:level` (EXAMPLE: /heat/severe)
--- routes/api.js
+++ routes/api.js
@@ -3,47 +3,110 @@ var restapi = express();
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('ClimateChange.db');
 
-// get all buildings (accessed at GET http://localhost:3000/api/building)
-restapi.get('/building', function(req, res){
-  var results = [];
-  db.each('SELECT * FROM building', function (err, result) {
-     results.push(result);
-  },
-  function () {
-     res.json(results);
-  });
+restapi.get('/:choice', function(req, res){
+
+    var results  = [];
+
+    if (req.params.choice == "index") { // List all areas with index
+      db.each("SELECT * FROM area", function (err, result) {
+         results.push(result);
+      },
+      function () {
+         res.json(results);
+      });
+
+    } else if (req.params.choice == "building" || req.params.choice == "quad" || req.params.choice == "parkinglot") { // List all areas
+
+      db.each("SELECT * FROM Area WHERE Type = ? collate nocase", req.params.choice, function (err, result) {
+       results.push(result);
+     },
+      function () {
+        res.json(results);
+     });
+
+    } else if (req.params.choice == "vulnerability") { // List all vulnerabilities
+
+        db.each("SELECT * FROM Vulnerability", function (err, result) {
+        results.push(result);
+    },
+    function () {
+       res.json(results);
+    });
+
+    } else {
+      res.json("Not valid entry /" + req.params.choice);
+    }
 });
 
-// get all green spaces 'quad' (accessed at GET http://localhost:3000/api/building)
-restapi.get('/quad', function(req, res){
-  var results = [];
-  db.each('SELECT * FROM quad', function (err, result) {
-     results.push(result);
-  },
-  function () {
-     res.json(results);
-  });
-});
-
-// get all buildings (accessed at GET http://localhost:3000/api/building)
-restapi.get('/parkinglot', function(req, res){
-  var results = [];
-  db.each('SELECT * FROM parkinglot', function (err, result) {
-     results.push(result);
-  },
-  function () {
-     res.json(results);
-  });
-});
-
-
-// get the building with that id (accessed at GET http://localhost:3000/api/building/:building_id)
-restapi.get('/building/:building_id', function(req,res) {
-   var id = req.params.building_id;
-   db.get('SELECT * FROM building WHERE BuildingID = ?', id, function (err, result) {
-     res.json(result);
+/* get area by id (accessed at GET http://localhost:3000/api/area/:id)
+ * Available input to list a specific entity:
+ *    /api/area/:id (EXAMPLE: 555HA)
+ */
+restapi.get('/area/:area_id', function(req,res) {
+   var results = [];
+   db.each("SELECT Area.Name AS 'Area Name', FloorLevel AS 'Floor Level', Room.Name AS 'Room Name', ONE.ID AS 'Storm Surge Severity', THREE.ID AS 'Extreme Heat Severity', TWO.ID AS 'Flooding Severity' FROM AreaFloor JOIN Floor ON AreaFloor.FloorID = Floor.ID JOIN Area ON AreaFloor.AreaID = Area.ID LEFT OUTER JOIN Room ON AreaFloor.RoomID = Room.ID JOIN Vulnerability SS ON AreaFloor.VulnerabilityType1 = SS.ID JOIN Vulnerability HT ON AreaFloor.VulnerabilityType2 = HT.ID JOIN Vulnerability FL ON AreaFloor.VulnerabilityType3 = FL.ID JOIN Severity ONE ON AreaFloor.StormSurgeSeverity = ONE.ID JOIN Severity TWO ON AreaFloor.FloodingSeverity = TWO.ID JOIN Severity THREE ON AreaFloor.HeatSeverity = THREE.ID WHERE AreaFloor.AreaID = ? collate nocase",
+    req.params.area_id, function (err, result) {
+      results.push(result);
+   },
+   function () {
+      if (results.length <= 0) {
+        res.json("/" + req.params.area_id + " is not a valid entry");
+      } else {
+        res.json(results);
+      }
    });
 });
 
+/* List areas by vulnerability type and level (accessed at GET http://localhost:3000/api/vulnerability/:type/:level)
+ * Available input to list a specific entity:
+ *    /api/vulnerability/:type/:level (EXAMPLE: /heat/severe)
+ */
+restapi.get('/vulnerability/:type/:level', function(req,res) {
+
+  var results = [];
+  // Check for the right vulnerability levels
+  if (req.params.level == "moderate-severe" || req.params.level == "low-moderate" || req.params.level == "none" || req.params.level == "low" || req.params.level == "moderate" || req.params.level == "severe") {
+    // Check for the right vulnerability types
+    if (req.params.type == "heat") {
+
+      db.each("SELECT Area.Name AS 'Area Name', FloorLevel AS 'Floor Level', Room.Name AS 'Room Name', THREE.ID AS 'Extreme Heat Severity' FROM AreaFloor JOIN Floor ON AreaFloor.FloorID = Floor.ID JOIN Area ON AreaFloor.AreaID = Area.ID LEFT OUTER JOIN Room ON AreaFloor.RoomID = Room.ID JOIN Vulnerability SS ON AreaFloor.VulnerabilityType1 = SS.ID JOIN Vulnerability HT ON AreaFloor.VulnerabilityType2 = HT.ID JOIN Vulnerability FL ON AreaFloor.VulnerabilityType3 = FL.ID JOIN Severity ONE ON AreaFloor.StormSurgeSeverity = ONE.ID JOIN Severity TWO ON AreaFloor.FloodingSeverity = TWO.ID JOIN Severity THREE ON AreaFloor.HeatSeverity = THREE.ID WHERE AreaFloor.HeatSeverity = ? collate nocase",
+       req.params.level, function (err, result) {
+         results.push(result);
+      },
+      function () {
+         res.json(results);
+      });
+    }
+    else if (req.params.type == "stormsurge") {
+
+      db.each("SELECT Area.Name AS 'Area Name', FloorLevel AS 'Floor Level', Room.Name AS 'Room Name', ONE.ID AS 'Storm Surge Severity' FROM AreaFloor JOIN Floor ON AreaFloor.FloorID = Floor.ID JOIN Area ON AreaFloor.AreaID = Area.ID LEFT OUTER JOIN Room ON AreaFloor.RoomID = Room.ID JOIN Vulnerability SS ON AreaFloor.VulnerabilityType1 = SS.ID JOIN Vulnerability HT ON AreaFloor.VulnerabilityType2 = HT.ID JOIN Vulnerability FL ON AreaFloor.VulnerabilityType3 = FL.ID JOIN Severity ONE ON AreaFloor.StormSurgeSeverity = ONE.ID JOIN Severity TWO ON AreaFloor.FloodingSeverity = TWO.ID JOIN Severity THREE ON AreaFloor.HeatSeverity = THREE.ID WHERE AreaFloor.StormSurgeSeverity = ? collate nocase",
+        req.params.level, function (err, result) {
+         results.push(result);
+      },
+      function () {
+         res.json(results);
+      });
+    }
+    else if (req.params.type == "flooding") {
+
+      db.each("SELECT Area.Name AS 'Area Name', FloorLevel AS 'Floor Level', Room.Name AS 'Room Name', TWO.ID AS 'Flooding Severity' FROM AreaFloor JOIN Floor ON AreaFloor.FloorID = Floor.ID JOIN Area ON AreaFloor.AreaID = Area.ID LEFT OUTER JOIN Room ON AreaFloor.RoomID = Room.ID JOIN Vulnerability SS ON AreaFloor.VulnerabilityType1 = SS.ID JOIN Vulnerability HT ON AreaFloor.VulnerabilityType2 = HT.ID JOIN Vulnerability FL ON AreaFloor.VulnerabilityType3 = FL.ID JOIN Severity ONE ON AreaFloor.StormSurgeSeverity = ONE.ID JOIN Severity TWO ON AreaFloor.FloodingSeverity = TWO.ID JOIN Severity THREE ON AreaFloor.HeatSeverity = THREE.ID WHERE AreaFloor.FloodingSeverity = ? collate nocase",
+        req.params.level, function (err, result) {
+         results.push(result);
+      },
+      function () {
+         res.json(results);
+      });
+    }
+    else {
+      res.json("/" + req.params.type + ". Select one of the following vulnerabilities: /heat /stormsurge /flooding");
+    }
+  }
+  else {
+    res.json("/" + req.params.level + ". Select one of the following vulnerability levels: /moderate-severe /low-moderate /none /low /moderate /severe");
+  }
+
+
+
+});
 
 module.exports = restapi;
